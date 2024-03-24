30be17438df423015c886c90bbe4c497c17a6e32
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-03-24 11:36:31 +0530

    First commit

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,17 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl, url }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <img src={imgUrl} />
+        </a>
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  )
+}
