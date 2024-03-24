9f2c49f5f326ea587366ae427913633415908225
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-03-24 14:12:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+
 import { Footer } from "./components/Footer";
-import AchievementsSection from './components/Achievements';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <AchievementsSection/>
       <Skills />
       <Projects />
       <Footer />
